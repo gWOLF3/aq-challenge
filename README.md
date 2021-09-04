cb3f4623eb7a0011d5f1a8bb150a7189665b00db
g <gWOLF3@protonmail.com>
2021-09-04 17:48:42 -0500

    kang login and auth, init sql db - kang loging and auth template - - json webtokens for auth - init sql db with docker server - - simple user db - boilerplate login screen

--- README.md
+++ README.md
@@ -1,5 +1,24 @@
 # AdQuick Code Challenge
 
+## Requirements: 
+
+### Billboard Voting
+This challenge is to build a web app that allows users to vote on billboards.
+
+Here's an outline of what your application should do:
+
+- Your app should ingest this csv as seed data. It contains a list billboards with accompanying images.
+- Users should be able to sign up & login (feel free to keep auth simple, we don't care to test your Devise installation skills).
+- The user should see a list of billboards in rank order along with their photos, names, score, and a way to vote the billboard up or down.
+- Voting should happen through ajax.
+- After voting, the user should see a success message.
+- Billboards should be ranked according to a basic time-decay algorithm
+- The user should be able to change their vote. They should only be able to assign 1 vote per board.
+- The app should look and feel good, as if it were made for real users. It should be responsive.
+- We expect the challenge to take about 3 hours. If you get past about 3.5 hours, please commit what you have finished so far and submit it.
+- The application should have a README that explains clearly how to set it up and run it. Do us a favor and make setup easy!
+- Note: all code should be written with reasonable performance in mind.
+
 ## Techonologies Used:
 
 ### Languages:
@@ -8,8 +27,14 @@
 - EJS Templates
 - CSS
 
+### Database
+- PostgreSQL
+
 ### Packages:
+
 #### Backend (NPM)
+- jsonwebtoken
+- postgres
 - express
 - ejs
 - axios
@@ -21,6 +46,11 @@
 #### Frontend (CSS/JS)
 - bootstrap
 - jquery
+- shajs
+
+### Other
+- Docker 
+- homebrew (MacOSX)
 
 
 
--- index.js
+++ index.js
@@ -3,11 +3,19 @@ const envalid = require('envalid')
 const { str, port, bool } = envalid
 const env = envalid.cleanEnv(process.env, {
   LISTEN_PORT: port(),
+  PGDATABASE: str(),
+  PGHOST: str(),
+  PGPORT: port(),
+  PGUSER: str(),
+  PGPASSWORD: str(),
+  JWT_PRIVATE_KEY: str(),
 })
+var jwt = require('jsonwebtoken')
 const express = require('express')
 const axios = require('axios')
 const cookieParser = require('cookie-parser')
 const fs = require('fs')
+const postgres = require('postgres')
 
 // express config
 const app = express()
@@ -19,15 +27,143 @@ app.use(express.static('pages'))
 app.use(express.static('assets'))
 
 
+// database stuff
+const sql = postgres(
+  `postgres://${env.PGUSER}:${env.PGPASSWORD}@${env.PGHOST}:${env.PGPORT}/${env.PGDATABASE}`
+)
+
+const preparePGDB = async () => {
+  await sql`
+  CREATE TABLE IF NOT EXISTS users (
+    userid SERIAL PRIMARY KEY, 
+    email VARCHAR (255) UNIQUE NOT NULL, 
+    password VARCHAR (64) NOT NULL,
+    alias VARCHAR (40)
+  )
+  `
+}
+const registerUser = async (u) => {
+  console.log('registerUser', u)
+  const [user] = await sql`
+    INSERT INTO users (
+      email,
+      alias,
+      password      
+    ) VALUES (
+      ${u.email},
+      ${u.alias},
+      ${u.password}
+    )
+  `
+  return user
+}
+
+// db helpers
+const loadInfo = async (email) => {
+  console.log('attempt loadInfo() for', email)
+  const [user] = await sql`SELECT * FROM users WHERE email=${email}`
+  console.log('successfully found info', user)
+  return user
+}
+
+// logout of account and expire the login cookie
+app.get('/logout', (req, res) => {
+  let o = {}
+  res.cookie('jwt', 'null', { expire: Date.now() })
+  res.redirect('../')
+})
+
+// login to account (receive cookie)
+app.post('/login', async (req, res) => {
+  let o = {}
+  const email = req.body.email
+  let userInfo
+  try {
+    userInfo = await loadInfo(email)
+    if (userInfo.password === req.body.password) {
+      const yourjwt = jwt.sign(
+        {
+          user_email: req.body.email,
+        },
+        env.JWT_PRIVATE_KEY,
+        {
+          expiresIn: 600000,
+        }
+      )
+      res
+        .cookie('jwt', yourjwt, {
+          maxAge: 600000,
+        })
+        .redirect('../')
+    } else {
+      o.error = 'password invalid'
+      res.status(420).render('login', o)
+    }
+  } catch (e) {
+    console.error(`problem loading info for ${email}`, e)
+    o.error = 'user not found'
+    res.status(666).render('login', o)
+  }
+})
+
+// create new account
+app.post('/register', async (req, res) => {
+  let o = {}
+  const user = {
+    email: req.body.email,
+    password: req.body.password,
+    alias: req.body.alias,
+  }
+  try {
+    await registerUser(user)
+    const yourjwt = jwt.sign(
+      {
+        user_email: req.body.email,
+      },
+      env.JWT_PRIVATE_KEY,
+      {
+        expiresIn: 600000,
+      }
+    )
+    res.cookie('jwt', yourjwt, {
+      maxAge: 600000,
+    })
+    res.status(200).redirect('../')
+  } catch (e) {
+    console.error(`problem registering user ${user.email}`, e)
+  }
+})
+
+// get home page
 app.get('/', async (req, res) => {
-  res.render('home')
+  let o = {}
+  if (req.cookies && req.cookies.jwt && req.cookies.jwt !== 'null') {
+    try {
+      // verify that we signed the JWT
+      const decodedJWT = await jwt.verify(req.cookies.jwt, env.JWT_PRIVATE_KEY)
+      const r = await loadInfo(decodedJWT.user_email)
+      o.email = decodedJWT.user_email
+      o.alias = r.alias
+      res.render('home', o)
+    } catch (e) {
+      console.log('issue while loading user info from jwt', e)
+      o.error = 'session expired. please relogin.'
+      res.render('login', o)
+    }
+  } else {
+    // no valid JWT, redirect to login
+    res.render('login', o)
+  }
 })
 
 const start = async () => {
   try {
+    await preparePGDB()
     app.listen(env.LISTEN_PORT)
     console.log('app is running on', env.LISTEN_PORT)
   } catch (e) {
+    console.error('bubbled error', e)
+    process.exit(420)
   }
 }
 
